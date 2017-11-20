9622ee2b93933289b6a42536ef55db311a9e3c3b
Melissa Mepham <m.a.mepham@gmail.com>
2017-11-20 19:34:59 +1300

    Eloquent

---
+++ eloquent-2.js
@@ -0,0 +1,51 @@
+Looping a triangle // OK I am just going to express how AMAZINGLY STOKED I AM THAT I ACTUALLY FIGURED SOMETHING OUT!
+
+var counter = 0;
+var x = "#";
+
+ while (counter < 7) {
+   console.log(x);
+   x = x + '#';
+   counter ++;
+ }
+
+ FizzBuzz // Check it out!! I DID IT AGAIN!
+
+ for (var number = 1; number <= 100; number = number + 1)
+
+ if (number % 3 === 0 && number % 5 === 0) {
+     console.log("FizzBuzz");
+    }
+     else if (number % 3 === 0 ) {
+     console.log("Fizz");
+   } else if
+     (number % 5 === 0 ) {
+     console.log("Buzz");
+   } else {
+     console.log(number);
+   }
+
+
+
+   var size = 8;
+var a
+var b
+
+
+Chess board // YES!
+
+
+var size = 8;
+var a
+var b
+var c = " "
+var d = "#"
+
+
+for (a = 0; a < size / 2; a++) {
+  console.log(" #".repeat(size/2));
+
+   for (b = 0; b < size / size; b++) {
+      console.log("# ".repeat(size/2));
+    }
+    }
